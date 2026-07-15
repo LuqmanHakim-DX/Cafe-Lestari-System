35124a74ec2814566d5b809a3aae73df5b18461c
205428178-Firdaus <2025428178@student.uitm.edu.my>
2026-07-16 01:20:40 +0800

    mobile view

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'lestari-cafe-v1';
+const ASSETS_TO_CACHE = [
+  '/index.html',
+  '/stylesheet.css',
+  '/screens/homepage.html',
+  '/screens/menu.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
